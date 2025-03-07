77e0bb6ac91ec8dff41bc1bdfbe4c154afbcbc32
rip-on <shipon.ripon123@gmail.com>
2025-03-07 09:15:39 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("hasan")
+
+let name = "Jahid"
+let age = 22
+let isLoggedIn = false
+let state; //undefined
+
+//number => 2 to power 53
+//bigint
+//string => "" (double qoute preferable but we can use single qoute)
+//boolean => true/false
+//null => standalone value //representation of empty value and it's not 0(zero)
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof "ripon")
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
