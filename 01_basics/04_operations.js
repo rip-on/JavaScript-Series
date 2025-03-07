45631c884b9b805775ce73545a04f9d5b2746193
rip-on <shipon.ripon123@gmail.com>
2025-03-07 10:32:41 +0000

    conversion operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,48 @@
+let value = 3
+let negValue =-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // raise to power
+// console.log(2/2);
+// console.log(2%2); //remainder
+
+// let str1 = "hello"
+// let str2 = " Jahid"
+// let str3 = str1 + str2
+// console.log(str3) //hello Jahid
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "3"); //33
+// console.log("1" + 2);
+
+//Tricky Question
+
+// console.log(true);
+// console.log(+true);//1
+// console.log(true+);//error
+// console.log(+"");//0
+
+//prefix and postfix
+
+// let gameCounter = 100;
+// gameCounter++
+// console.log(gameCounter);//101
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);//101
+
+let x = 3 
+y = x++
+console.log(x); //4
+console.log(y); //3
+
+let a = 5
+b = ++a
+console.log(b); //6
+console.log(a); //6
