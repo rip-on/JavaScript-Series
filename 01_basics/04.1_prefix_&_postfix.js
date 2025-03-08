a3afa1b69b86f1ee216ad04fdae2d3fdbde1e357
rip-on <shipon.ripon123@gmail.com>
2025-03-08 13:16:10 +0000

    prefix and postfix/ increment & decrement

---
+++ 01_basics/04.1_prefix_&_postfix.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = a++;
+console.log(a)
+console.log(b)
+
+
+let x = 5;
+let y = ++x;
+console.log(x);
+console.log(y);
+
+
+let p = 3;
+let q = p--;
+console.log(p);
+console.log(q);
+
+
+let m = 4;
+let n = --m;
+console.log(m);
+console.log(n);
