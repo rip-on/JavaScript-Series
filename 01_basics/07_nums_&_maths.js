97cc2b1cdbf9d6a6c4aecd7768915b56473782fb
rip-on <shipon.ripon123@gmail.com>
2025-03-19 07:41:31 +0600

    random function

--- 01_basics/07_nums_&_maths.js
+++ 01_basics/07_nums_&_maths.js
@@ -34,6 +34,27 @@ console.log(hundreds.toLocaleString('en-BD'));//1,000,000 Bangladeshi Standard('
 //156523475;
 
 console.log(Math)
+console.log(Math.abs(-4)); //4
+console.log(Math.round(4.3)); //4
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.2));//5 Just a little more than 4
+console.log(Math.floor(4.9));//4 
+console.log(Math.min(4,3,6,8)); //3
+console.log(Math.max(4,3,6,8)); //8
+
+console.log(Math.random()); //value from 0 and 1
+console.log(Math.random()*10);// shifting value from left to right in 1 digit, 
+console.log((Math.random()*10) + 1 );//but what if the value 0.075434, so we add 1 to avoid the case;
+console.log(Math.floor(Math.random()*10) + 1 );//but what if the value 0.075434, so we add 1 to avoid the case;
+//now we have gurantee that minimum value will be 1 not zero.
+
+min = 10;
+max = 20;
+console.log(Math.floor(Math.random() * (max-min + 1) + min))
+
+
+
+
 
 
 
