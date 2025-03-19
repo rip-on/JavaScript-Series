63d8a3a9ee1d13d348c215892f6519c32a409802
rip-on <shipon.ripon123@gmail.com>
2025-03-19 06:15:50 +0600

    running

---
+++ 07_nums_&_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+console.log(score);//400
+
+const balance = new Number(100)
+console.log(balance);//[Number: 100]
+
+console.log(balance.toString());//string
+console.log(balance.toString().length);//3
+console.log(balance.toFixed(2));//100.00
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));//23.9
+
+const otNum = 123.8966
+
+console.log(otNum.toPrecision(3));//124
+
+const otheNum = 1123.8966
+
+console.log(otheNum.toPrecision(3));//1.12e+3
+
+const othNum = 123.8966
+
+console.log(othNum.toPrecision(4));//123.9
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString());//1,000,000 US-Standard
+console.log(hundreds.toLocaleString('en-IN'));//10,00,000 Indian Standard
+console.log(hundreds.toLocaleString('en-BD'));//1,000,000 Bangladeshi Standard('bn-BD')
+
+//+++++++++++++++++++++++ Math ++++++++++++++++++++
+//156523475;
+
+
+
+
+
+
+
