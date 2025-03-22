576c59c77969cf5a65abc716d75689528d177d73
rip-on <shipon.ripon123@gmail.com>
2025-03-22 10:43:15 +0600

    destructer object/json formatter

---
+++ 02_basics/05_object_III.js
@@ -0,0 +1,31 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    couseInstructor: "Shipon choudhury"
+}
+
+// course.couseInstructor 
+const {couseInstructor} = course//<--from where we want data
+
+console.log(couseInstructor);
+
+const {couseInstructor: instructor} = course//we define a name in that case instructor// destructering
+
+// console.log(instructor);
+
+
+// const navbar = (company)=>{
+
+// } 
+// navbar(company = "Mehedi") //only for react
+
+//json
+
+// {
+//     "name": "jahid",
+//     "coursename": "js in hindi",
+//     "price":
+// }
+//https://api.github.com/users/rip-on
+//https://randomuser.me/
+//https://jsonformatter.org/ ---> code / tree / 
