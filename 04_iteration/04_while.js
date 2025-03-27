f576587666d25fcb5f78f9757e19238909719cc0
rip-on <shipon.ripon123@gmail.com>
2025-03-27 18:08:52 +0600

    while loops in array and normal itself

---
+++ 04_iteration/04_while.js
@@ -0,0 +1,27 @@
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+// Value of index is 0
+// Value of index is 2
+// Value of index is 4
+// Value of index is 6
+// Value of index is 8
+// Value of index is 10
+
+let myArray = ["superman", "flash", "batman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+// Value is superman
+// Value is flash
+// Value is batman  
+
