1a3e54d08d5e9bdda4782244f7fec1dc0361eac8
rip-on <shipon.ripon123@gmail.com>
2025-03-29 16:31:41 +0600

    DOM intro to Deep

---
+++ 05_dom/01_dom.js
@@ -0,0 +1,17 @@
+//DOM = Document Object Model
+
+//in console window we can access by console.log(window)
+//console.log(document) to access body & it's dependencies
+//console.dir(document) to access all properties
+//Brendan Eich
+
+//console.log(document.baseURL)// en.wikipedia
+//console.log(document.links) // how many links are there
+//console.log(document.links[2]) // as array
+//document.getElementById('firstHeading')
+//.
+//.
+//document.getElementById('firstHeading').innerHTML = "<h1>Jahid Hasan</h1>"
+
+//https://webwhiteboard.com/
+//https://excalidraw.com/
